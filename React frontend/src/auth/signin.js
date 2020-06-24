b8e390902ebfb8a30c85c3c95191c24d43bd022d
Ayush Mishra <46623370+AyushWebDev@users.noreply.github.com>
2020-06-24 23:55:07 +0530

    Add files via upload

--- React frontend/src/auth/signin.js
+++ React frontend/src/auth/signin.js
@@ -1,31 +1,71 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import {Link,Redirect} from 'react-router-dom';
+import {signin,authenticate} from './api';
 
 class Signin extends Component{
     constructor(){
         super();
         this.state={
             email: "",
-            password: ""
+            password: "",
+            error: "",
+            redirectToChat: false
         }
     }
 
     handleChange=name=>event=>{
         event.preventDefault();
-        this.setState={
+        this.setState({
             [name]: event.target.value
-        }
+        })
     }
 
-   
+    clickSubmit=event=>{
+        event.preventDefault();
+        const {email,password}=this.state;
+        const user={
+            email,
+            password
+        }
+        signin(user)
+        .then(data=>{
+            if(data.error)
+            {
+                this.setState({
+                    error: data.error
+                })
+            }
+            else
+            {
+                authenticate(data,()=>{
+                    this.setState({
+                        email: "",
+                        password: "",
+                        redirectToChat: true
+                    })
+                })
+                
+            }
+        })
+    }
 
     render(){
+        if(this.state.redirectToChat)
+            return <Redirect to="/chat"/>
+        
         return(
             <div className="container">
                 <div className="row" style={{marginTop: "100px"}}>
                 <div className="col-md-4"></div>
                 <div className="col-md-4" style={{boxShadow: "1px 1px 10px"}}>
-                    <form>
+                       <div className="bg-success">
+                            <h4 style={{color: "white",textAlign: "center"}}>Sign In</h4>
+                        </div>
+                    <form style={{padding: "50px 20px"}}>
+                        {this.state.error &&
+                        <div className="alert alert-danger">{this.state.error}</div>
+                        }
+                        
                         <div className="form-group">
                             <input type="text" className="form-control" placeholder="Email" onChange={this.handleChange("email")}></input>  
                         </div>
@@ -35,7 +75,7 @@ class Signin extends Component{
                         <div>
                             <p>Didn't Registered Yet?<span className="float-right btn btn-success"><Link to="/signup">Sign Up</Link></span></p>
                         </div>
-                        <button className="btn btn-raised btn-success">Sign In</button>
+                        <button className="btn btn-raised btn-success" onClick={this.clickSubmit}>Sign In</button>
                     </form>
                 </div>
                 </div>
--- React frontend/src/auth/signup.js
+++ React frontend/src/auth/signup.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {signup} from './api';
 
 class Signup extends Component{
     constructor(){
@@ -7,18 +8,48 @@ class Signup extends Component{
         this.state={
             name: "",
             email: "",
-            password: ""
+            password: "",
+            error: "",
+            signedup: false
         }
     }
 
     handleChange=name=>event=>{
         event.preventDefault();
-        this.setState={
-            [name]: event.target.value
-        }
+        this.setState({
+            [name]: event.target.value,
+            error: ""
+        })
     }
 
-   
+    clickSubmit=event=>{
+        event.preventDefault();
+        const {name,email,password}=this.state;
+        const user={
+            name,
+            email,
+            password
+        }
+
+        signup(user)
+        .then(data=>{
+            if(data.error)
+            {
+                this.setState({
+                    error: data.error
+                })
+            }
+            else
+            {
+                this.setState({
+                    name: "",
+                    email: "",
+                    password: "",
+                    signedup: true
+                })
+            }
+        })
+    }
 
     render(){
         return(
@@ -26,7 +57,18 @@ class Signup extends Component{
                 <div className="row" style={{marginTop: "100px"}}>
                 <div className="col-md-4"></div>
                 <div className="col-md-4" style={{boxShadow: "1px 1px 10px"}}>
-                    <form>
+                        <div className="bg-success">
+                            <h4 style={{color: "white",textAlign: "center"}}>Sign Up</h4>
+                        </div>
+                    <form  style={{padding: "50px 20px"}}>
+                        {this.state.error &&
+                        <div className="alert alert-danger">{this.state.error}</div>
+                        }
+
+                        {this.state.signedup &&
+                        <div className="alert alert-success">Signed Up Succesfully<span className="float-right"><Link to="/">Signin</Link></span></div>
+                        }
+
                         <div className="form-group">
                             <input type="text" className="form-control" placeholder="Name" onChange={this.handleChange("name")}></input>  
                         </div>
@@ -39,7 +81,7 @@ class Signup extends Component{
                         <div>
                             <p>Already Registered?<span className="float-right btn btn-success"><Link to="/">Sign In</Link></span></p>
                         </div>
-                        <button className="btn btn-raised btn-success">Sign Up</button>
+                        <button className="btn btn-raised btn-success" onClick={this.clickSubmit}>Sign Up</button>
                     </form>
                 </div>
                 </div>
